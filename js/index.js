43a7d523b579e6cd3968a23a7f266dec717f6ed1
Ming <ming.ming@live.de>
2022-03-22 07:30:13 +0100

    anzeigen von daten

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@
         var staionId = $("#stationId").val();
         console.log(staionId);
         $.get("api.php?r=/station/" + staionId + "/measurement", function(data) {
+            $('#measurements').html(parseMeasurementTable(data));
 
             // $.each(data, function(index, measurements) {
             //     temperature.push(measurements.temperature);
@@ -53,4 +54,22 @@
             time = [];
             rain = [];
         });
+        
+    }
+
+    function parseMeasurementTable(data){
+        var tmp = "";
+        $.each(data, function(i,measurement){
+            tmp += "<tr>";
+            tmp += "<td>" + measurement.time + "</td>";
+            tmp += "<td>" + measurement.temperature + "</td>";
+            tmp += "<td>" + measurement.rain + "</td>";
+            tmp += "<td>";
+            tmp += '<a class="btn btn-info" href="index.php?r=measurement/view&id=' + measurement.id + '"><span class="glyphicon glyphicon-eye-open"></span></a>&nbsp;'
+            tmp += '<a class="btn btn-primary" href="index.php?r=measurement/update&id=' + measurement.id + '"><span class="glyphicon glyphicon-pencil"></span></a>&nbsp;'
+            tmp += '<a class="btn btn-danger" href="index.php?r=measurement/delete&id=' + measurement.id + '"><span class="glyphicon glyphicon-remove"></span></a>';
+            tmp += "</td>";
+            tmp += "</tr>"
+        });
+        return tmp;
     }
