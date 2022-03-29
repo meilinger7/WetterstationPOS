a9e05db723f76b3cfabacc453ad471a1ca1640d2
Ming <ming.ming@live.de>
2022-03-29 10:42:27 +0200

    Station create, delete implemented

--- controllers/StationController.php
+++ controllers/StationController.php
@@ -21,6 +21,8 @@ class StationController extends Controller
             $this->actionDelete($id);
         } elseif ($operation == 'index'){
             $this->actionIndex();
+        } elseif ($operation == 'create'){
+            $this->actionCreate();
         } else {
             Controller::showError("Page not found", "Page for operation " . $operation . " was not found!");
         }
@@ -40,7 +42,19 @@ class StationController extends Controller
 
     public function actionCreate()
     {
+        $model = new Station;
+        if (!empty($_POST)) {
+            $model->setName($this->getDataOrNull('name'));
+            $model->setAltitude($this->getDataOrNull('altitude'));
+            $model->setLocation($this->getDataOrNull('location'));
 
+            if ($model->save()) {
+                $this->redirect('station/index');
+                return;
+            }
+        }
+
+        $this->render('station/create', $model);
     }
 
     public function actionUpdate($id)
@@ -48,10 +62,9 @@ class StationController extends Controller
         $model =Station::get($id);
 
         if (!empty($_POST)) {
-            $model->setTime($this->getDataOrNull('time'));
-            $model->setTemperature($this->getDataOrNull('temperature'));
-            $model->setRain($this->getDataOrNull('rain'));
-            $model->setStationId($this->getDataOrNull('station_id'));
+            $model->setName($this->getDataOrNull('name'));
+            $model->setAltitude($this->getDataOrNull('altitude'));
+            $model->setLocation($this->getDataOrNull('location'));
 
             if ($model->save()) {
                 $this->redirect('station/view&id=' . $model->getId());
@@ -64,7 +77,21 @@ class StationController extends Controller
 
     public function actionDelete($id)
     {
+        $model = Station::get($id);
 
+        if ($model == null) {
+            Controller::showError("Page not found", "Data not found", 404);
+        } else {
+            if (!empty($_POST)) {
+                Station::delete($id);
+                $this->redirect('station/delete');
+                return;
+            }
+
+            $this->render('station/delete', Station::get($id));
+        }
     }
+
+    
 
 }
